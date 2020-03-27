b8fc43a7a9dd60d8ff9891fba4ec63eccaa147a9
Maurice <mauricelecordier@gmail.com>
2020-03-27 21:29:06 +0200

    added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import './App.css';
+// @ts-ignore
 import githubLink from './GitHub-Mark-Light-32px.png';
 // Todo - Swap out the github image link for a proper icon
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className='App-header'>
-        <h1>Maurice Le Cordier</h1>
-        <h3>Welcome to my slice of the Web</h3>
+    <div className="bg-gray-900 w-screen h-screen flex flex-col items-center justify-center">
+        <p className='text-6xl text-gray-300'>Maurice Le Cordier</p>
+        <p className='text-5xl mb-1 text-gray-300'>Welcome to my slice of the Web</p>
         <a href={'https://github.com/mauriceLC92'}>
           <img src={githubLink} alt="github link" />
         </a>
-      </header>
     </div>
   );
 }
