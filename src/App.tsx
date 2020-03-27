c626d158cc8c4849aafbdbda506ca57d021561fe
Maurice <mauricelecordier@gmail.com>
2020-03-27 21:41:32 +0200

    testing with postbuild command

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const App: React.FC = () => {
   return (
     <div className="bg-gray-900 w-screen h-screen flex flex-col items-center justify-center">
         <p className='text-6xl text-gray-300'>Maurice Le Cordier</p>
-        <p className='text-5xl mb-1 text-gray-300'>Welcome to my slice of the Web</p>
+        <p className='text-5xl mb-1 text-gray-300'>Welcome to my slice of the Web :)</p>
         <a href={'https://github.com/mauriceLC92'}>
           <img src={githubLink} alt="github link" />
         </a>
